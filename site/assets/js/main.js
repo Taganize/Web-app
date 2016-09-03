3f88105b9e5a209d302dbba38297d438ffcab854
trachanh1609 <Vu@Users-MacBook-Pro.local>
2016-09-03 12:12:24 +0300

    Vu commits

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -19,18 +19,50 @@ app.config(function($routeProvider) {
         templateUrl: 'templates/tags.html',
         controller: 'tagsCtrl'
     })
-    .when('/', {
+    .when('/design', {
         templateUrl: 'templates/design.html',
         controller: 'designCtrl'
     })
     .otherwise({
     		redirectTo: '/'
     });
-});
 
-app.controller('loginCtrl', function($scope) {
 
 });
+
+app.controller('loginCtrl', function($scope,$location) {
+  $scope.login =function() {
+    if ($scope.email && $scope.password) {
+        var email = $scope.email;
+        var password = $scope.password;
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+        }).then(function(user){
+          if (user) {
+            $location.path('/tags');
+          } else {
+            alert("Error !");
+          }
+        });
+    }
+  }
+});
+
+app.directive("productTag", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-tag.html",
+      controller: function() {
+        
+      },
+      controllerAs: "tag"
+    };
+  });
+
+
 app.controller('signupCtrl', function($scope) {
 
 });
